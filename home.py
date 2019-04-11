ae7a1e27e1b8b2fb683ba84bbef0c8192f733e50
Pedro Burgos <pedroburgos@gmail.com>
2019-04-11 16:07:31 -0500

    another server fix

--- home.py
+++ home.py
@@ -23,3 +23,7 @@ def home():
         return render_template('index.html', **data)
     else:
         return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
